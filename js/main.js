631476a6da17c0e1dd3b0e019ea368852b723be6
huydzo <huydzo88@gmail.com>
2020-11-17 22:20:05 +0700

    - update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-$('document').ready(function () {
+/*Nav icon mobile*/
+function openNavigation (){
+    //Toggle Navigation
+    /*Nav icon mobile*/
+    $('#navtop_icon').click(function(){
+        $(this).toggleClass('open');
+        $('.navtop_collapse').toggleClass('open');
+        $('.overlay').toggleClass('open');
+        $('html , body').toggleClass('open_menu');
+    });
 
+    /* When user clicks outside */
+    $(".overlay").click(function() {
+        $(this).toggleClass('open');
+        $("#navtop_icon").toggleClass("open");
+        $(".navtop_collapse").toggleClass("open");
+        $('html , body').toggleClass('open_menu');
+    });
+}
+function preLoad (){
+    //Preloader
+    var preloader = $('#preloader');
+    $(window).on('load',function () {
+        preloader.fadeOut('slow');
+    });
+}
+$('document').ready(function () {
+    //Common
+    openNavigation();
+    preLoad();
 });

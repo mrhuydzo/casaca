de98e4b03b2766193bbcba42b9ecff4c3b8b420a
huydzo <huydzo88@gmail.com>
2020-11-21 23:54:05 +0700

    - update

--- js/main.js
+++ js/main.js
@@ -24,8 +24,45 @@ function preLoad (){
         preloader.fadeOut('slow');
     });
 }
+
+function gallery(){
+    // var swiper = new Swiper('.swiper-container', {
+    //     slidesPerView: 4,
+    //     spaceBetween: 25,
+    //     direction: 'vertical',
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         clickable: true,
+    //     },
+    // });
+
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        loop: true,
+        freeMode: true,
+        loopedSlides: 5, //looped slides should be the same
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        loop: true,
+        loopedSlides: 5, //looped slides should be the same
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs,
+        },
+    });
+}
+
 $('document').ready(function () {
     //Common
     openNavigation();
     preLoad();
+    gallery();
 });
